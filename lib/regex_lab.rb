ae15534f5b0b9d6d3b1586c4a2d705a40b8a3e6a
Christine Cua <cloudycua@gmail.com>
2018-03-01 20:50:57 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -22,8 +22,5 @@ end
 
 # "2438894546", "(718)891-1313", "234 435 9978", "(800)4261134"
 def valid_phone_number?(phone)
-  if phone.scan(/[0-9]/).count == 10
-    true
-  else false
-  end
+  phone.scan(/[0-9]/).count == 10 ? true : false
 end
